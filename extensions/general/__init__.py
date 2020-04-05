83a62df1b4f38ff47a11205fcc5a3d31e95cd277
Marti157 <unregistered@unregistered.com>
2020-04-05 23:53:17 +0200

    New contests feature

--- extensions/general/__init__.py
+++ extensions/general/__init__.py
@@ -1,6 +1,8 @@
 from .general import General
 from .profile import Profile
+from .contest import Contest
 
 def setup(fff):
     fff.add_cog(General(fff))
     fff.add_cog(Profile(fff))
+    fff.add_cog(Contest(fff))
